5c72450dd48ebbe64e9bc59490b186607bef270b
Glen Campbell <glen@broadpool.com>
2012-09-17 13:11:53 -0500

    regenerated docs

--- samples/auth.php
+++ samples/auth.php
@@ -1,6 +1,18 @@
 <?php
-// (c)2012 Rackspace Hosting
-// See COPYING for licensing information
+/**
+ * Authentication Sample
+ *
+ * @copyright 2012 Rackspace Hosting, Inc.
+ * See COPYING for licensing information
+ *
+ * @package samples
+ * @version 1.0.0
+ * @author Glen Campbell <glen.campbell@rackspace.com>
+ *
+ * A very simple example showing how to authenticate using the OpenCloud\OpenStack
+ * connection object. Replace values {in brackets} with your URL, username, and 
+ * password. 
+ */
 
 require_once('openstack.inc');
 
