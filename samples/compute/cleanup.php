5f077cfab89e3694af02f6f1b800795846e73083
Glen Campbell <glen@broadpool.com>
2012-09-26 14:02:29 -0500

    bug in rebuild logic

---
+++ samples/compute/cleanup.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * (c)2012 Rackspace Hosting. See COPYING for license details
+ *
+ * This sample creates an isolated network called SAMPLENET. It then
+ * creates two servers attached to that network. Once the servers are
+ * created, it pauses to wait for you to verify the connectivity. When
+ * it continues, it deletes the servers and SAMPLENET.
+ */
+$start = time();
+ini_set('include_path', './lib:'.ini_get('include_path'));
+require('rackspace.inc');
+define('INSTANCENAME', 'SmokeTestInstance');
+define('SERVERNAME', 'SmokeTestServer');
+
+/**
+ * Relies upon environment variable settings — these are the same environment
+ * variables that are used by python-novaclient. Just make sure that they're
+ * set to the right values before running this test.
+ */
+define('AUTHURL', 'https://identity.api.rackspacecloud.com/v2.0/');
+define('USERNAME', $_ENV['OS_USERNAME']);
+define('TENANT', $_ENV['OS_TENANT_NAME']);
+define('APIKEY', $_ENV['NOVA_API_KEY']);
+
+/**
+ * numbers each step
+ */
+function step($msg,$p1=NULL,$p2=NULL,$p3=NULL) {
+    global $STEPCOUNTER;
+    printf("\nStep %d. %s\n", ++$STEPCOUNTER, sprintf($msg,$p1,$p2,$p3));
+}
+function info($msg,$p1=NULL,$p2=NULL,$p3=NULL) {
+    printf("  %s\n", sprintf($msg,$p1,$p2,$p3));
+}
+define('TIMEFORMAT', 'r');
+
+print "This script deletes stuff created in other sample code scripts\n";
+
+step('Authenticate');
+$rackspace = new OpenCloud\Rackspace(AUTHURL,
+	array( 'username' => USERNAME,
+		   'tenantName' => TENANT,
+		   'apiKey' => APIKEY ));
+
+step('Connect to Cloud Servers');
+$cloudservers = $rackspace->Compute('cloudServersOpenStack', 'DFW');
+
+step('Deleting SmokeTestServer(s)');
+$list = $cloudservers->ServerList();
+while($server = $list->Next())
+    if ($server->name == 'SmokeTestServer') {
+        info('Deleting server [%s] %s', $server->id, $server->name);
+        $server->Delete();
+    }
+
+step('Deleting SAMPLENET and SMOKETEST Networks');
+$list = $cloudservers->NetworkList();
+while($network = $list->Next()) {
+    if ($network->label=='SAMPLENET' || $network->label=='SMOKETEST') {
+        info('Deleting network [%s] %s', $network->id, $network->label);
+        $network->Delete();
+    }
+}
+
