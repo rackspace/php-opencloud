8d6bc92ec1f6bf2a292a6ad8ada34e42ef5a5ac7
Glen Campbell <glen@broadpool.com>
2012-10-10 09:38:43 -0500

    added unit tests for CBS

---
+++ tests/VolumeTypeTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Unit Tests
+ *
+ * @copyright 2012 Rackspace Hosting, Inc.
+ * See COPYING for licensing information
+ *
+ * @version 1.0.0
+ * @author Glen Campbell <glen.campbell@rackspace.com>
+ */
+
+require_once('volumetype.inc');
+require_once('stub_conn.inc');
+require_once('volumeservice.inc');
+
+class publicVolumeType extends OpenCloud\VolumeService\VolumeType {
+    public function JsonName() { return parent::JsonName(); }
+    public function ResourceName() { return parent::ResourceName(); }
+}
+
+class VolumeTypeTest extends PHPUnit_Framework_TestCase
+{
+    private
+        $vt;
+    public function __construct() {
+        $conn = new StubConnection('http://example.com', 'SECRET');
+        $serv = new OpenCloud\VolumeService(
+            $conn, 'cloudBlockStorage', 'DFW', 'publicURL'
+        );
+        $this->vt = new publicVolumeType($serv);
+    }
+    /**
+     * Tests
+     */
+    /**
+     * @expectedException OpenCloud\CreateError
+     */
+    public function testCreate() {
+        $this->vt->Create();
+    }
+    /**
+     * @expectedException OpenCloud\UpdateError
+     */
+    public function testUpdate() {
+        $this->vt->Update();
+    }
+    /**
+     * @expectedException OpenCloud\DeleteError
+     */
+    public function testDelete() {
+        $this->vt->Delete();
+    }
+    public function testJsonName() {
+        $this->assertEquals(
+            'volume_type',
+            $this->vt->JsonName());
+    }
+    public function testResourceName() {
+        $this->assertEquals(
+            'types',
+            $this->vt->ResourceName());
+    }
+}
