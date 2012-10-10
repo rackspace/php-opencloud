e55b3f4f2587259fb0ad0eba88be94432e66307f
Glen Campbell <glen@broadpool.com>
2012-10-10 09:49:54 -0500

    unit tests for Snapshot class

---
+++ tests/SnapshotTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Unit Tests
+ *
+ * @copyright 2012 Rackspace Hosting, Inc.
+ * See COPYING for licensing information
+ *
+ * @version 1.0.0
+ * @author Glen Campbell <glen.campbell@rackspace.com>
+ */
+
+require_once('snapshot.inc');
+require_once('stub_conn.inc');
+require_once('volumeservice.inc');
+
+class publicSnapshot extends OpenCloud\VolumeService\Snapshot {
+    public function JsonName() { return parent::JsonName(); }
+    public function ResourceName() { return parent::ResourceName(); }
+    public function CreateJson() { return parent::CreateJson(); }
+}
+
+class SnapshotTest extends PHPUnit_Framework_TestCase
+{
+    private
+        $snap;
+    public function __construct() {
+        $conn = new StubConnection('http://example.com', 'SECRET');
+        $serv = new OpenCloud\VolumeService(
+            $conn, 'cloudBlockStorage', 'DFW', 'publicURL'
+        );
+        $this->snap = new publicSnapshot($serv);
+    }
+    /**
+     * Tests
+     */
+    /**
+     * @expectedException OpenCloud\UpdateError
+     */
+    public function testUpdate() {
+        $this->snap->Update();
+    }
+    public function testName() {
+        $this->snap->display_name = 'FOOBAR';
+        $this->assertEquals(
+            'FOOBAR',
+            $this->snap->Name());
+    }
+    public function testJsonName() {
+        $this->assertEquals(
+            'snapshot',
+            $this->snap->JsonName());
+    }
+    public function testResourceName() {
+        $this->assertEquals(
+            'os-snapshots',
+            $this->snap->ResourceName());
+    }
+    public function testCreateJson() {
+        $this->snap->display_name = 'BARFOO';
+        $obj = $this->snap->CreateJson();
+        $this->assertEquals(
+            'BARFOO',
+            $obj->snapshot->display_name);
+    }
+}
