8436b5ecef089ed71031f037dfeec0ce1c4fc481
Glen Campbell <glen@broadpool.com>
2012-10-11 15:47:24 -0500

    sample code for load balancer

---
+++ samples/loadbalancers/createlb.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * (c)2012 Rackspace Hosting. See COPYING for license details
+ *
+ */
+$start = time();
+ini_set('include_path', './lib:'.ini_get('include_path'));
+require('rackspace.inc');
+
+/**
+ * Relies upon environment variable settings — these are the same environment
+ * variables that are used by python-novaclient. Just make sure that they're
+ * set to the right values before running this test.
+ */
+define('AUTHURL', 'https://identity.api.rackspacecloud.com/v2.0/');
+define('USERNAME', $_ENV['OS_USERNAME']);
+define('TENANT', $_ENV['OS_TENANT_NAME']);
+define('APIKEY', $_ENV['NOVA_API_KEY']);
+
+define('VOLUMENAME', 'SampleVolume');
+define('VOLUMESIZE', 100);
+define('SERVERNAME', 'CBS-test-server');
+
+/**
+ * numbers each step
+ */
+function step($msg,$p1=NULL,$p2=NULL,$p3=NULL) {
+    global $STEPCOUNTER;
+    printf("\nStep %d. %s\n", ++$STEPCOUNTER, sprintf($msg,$p1,$p2,$p3));
+}
+function info($msg,$p1=NULL,$p2=NULL,$p3=NULL) {
+    printf("  %s\n", sprintf($msg,$p1,$p2,$p3));
+}
+define('TIMEFORMAT', 'r');
+
+step('Authenticate');
+$rackspace = new OpenCloud\Rackspace(AUTHURL,
+	array( 'username' => USERNAME,
+		   'tenantName' => TENANT,
+		   'apiKey' => APIKEY ));
+
+step('Connect to the Load Balancer Service');
+$lbservice = $rackspace->LoadBalancerService('cloudLoadBalancers', 'DFW');
+
+step('Create a Load Balancer');
+$lb = $lbservice->LoadBalancer();
+$lb->AddVirtualIp('public');
+$lb->AddNode('50.56.172.87', 80);
+setDebug(TRUE);
+$response = $lb->Create(array(
+    'name' => 'My-First-Load-Balancer',
+    'protocol' => 'HTTP',
+    'port' => 80));
+setDebug(FALSE);
+
+print_r($response);
+
+step('DONE');
+exit;
+
