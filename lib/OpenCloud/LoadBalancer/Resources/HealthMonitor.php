0004a0f464c1115d12ecba62b8b9b30e20bf0c75
Matt Martz <matt@sivel.net>
2013-04-09 07:27:01 -0500

    Update HealthMonitor to allow for manipulation

--- lib/OpenCloud/LoadBalancer/Resources/HealthMonitor.php
+++ lib/OpenCloud/LoadBalancer/Resources/HealthMonitor.php
@@ -5,12 +5,38 @@ namespace OpenCloud\LoadBalancer\Resources;
 /**
  * sub-resource to read health monitor info
  */
-class HealthMonitor extends Readonly {
+class HealthMonitor extends SubResource {
 	public
-		$type;
+		$type,
+		$delay,
+		$timeout,
+		$attemptsBeforeDeactivation,
+		$bodyRegex,
+		$hostHeader,
+		$path,
+		$statusRegex;
 	protected static
 		$json_name = 'healthMonitor',
 		$url_resource = 'healthmonitor';
+	protected
+		$_create_keys = array(
+			'type',
+			'delay',
+			'timeout',
+			'attemptsBeforeDeactivation',
+			'bodyRegex',
+			'hostHeader',
+			'path',
+			'statusRegex'
+		);
+	/**
+	 * creates a new health monitor
+	 *
+	 * This calls the Update() method, since it requires a PUT to create
+	 * a new error page. A POST request is not supported, since the URL
+	 * resource is already defined.
+	 *
+	 * @param array $parm array of parameters
+	 */
+	public function Create($parm=array()) { $this->Update($parm); }
 } // end HealthMonitor
-
-
