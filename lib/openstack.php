07e1d6ca3c7cefa354df6756f79fccb768a8a633
Glen Campbell <glen@broadpool.com>
2013-05-14 14:55:58 -0500

    updated license, contributors

--- lib/openstack.php
+++ lib/openstack.php
@@ -1,5 +1,8 @@
 <?php
 /**
  * provided for backwards compatibility
+ *
+ * @copyright 2013 Rackspace Hosting, Inc.
+ * @license http://www.apache.org/licenses/LICENSE-2.0
  */
 require_once dirname(__FILE__).'/php-opencloud.php';
--- lib/php-opencloud.php
+++ lib/php-opencloud.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * entry point for PHP-OpenCloud library
+ *
+ * @copyright 2013 Rackspace Hosting, Inc.
+ * @license http://www.apache.org/licenses/LICENSE-2.0
  */
 require_once(__DIR__ . '/Autoload.php');
 require_once(__DIR__ . '/OpenCloud/Globals.php');
