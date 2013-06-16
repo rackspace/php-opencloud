d65cd5b05a009d0aa0b9762cc81cb8c19e046368
jamiehannaford <jamie@limetree.org>
2013-06-16 20:58:01 +0100

    Deleting superfluous doc file

--- lib/OpenCloud/CloudMonitoring/Resource/AgentHostInfo.php
+++ lib/OpenCloud/CloudMonitoring/Resource/AgentHostInfo.php
@@ -5,7 +5,7 @@ namespace OpenCloud\CloudMonitoring\Resource;
 class AgentHostInfo extends AgentHost
 {
 	
-	public function SetProperty($property, $value)
+	public function SetProperty($property, $value, array $prefixes = array())
 	{
 		$this->$property = $value;
 	}
