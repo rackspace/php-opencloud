0685a1e849b49896adb2f504a99e841e6d711faf
John Nicholls <github-john@intobikes.co.uk>
2013-06-22 17:23:45 +0200

    Update openstack.php
    consistency. use __DIR__

--- lib/openstack.php
+++ lib/openstack.php
@@ -5,4 +5,4 @@
  * @copyright 2013 Rackspace Hosting, Inc.
  * @license http://www.apache.org/licenses/LICENSE-2.0
  */
-require_once dirname(__FILE__).'/php-opencloud.php';
+require_once __DIR__.'/php-opencloud.php';
