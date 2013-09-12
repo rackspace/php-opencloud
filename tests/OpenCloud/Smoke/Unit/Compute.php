85012adc2b00403cad8ab3e58ed1e4d06acb0600
Jamie H <jamie@limetree.org>
2013-09-12 13:47:51 +0200

    Beginning work on refactoring smoke tests

---
+++ tests/OpenCloud/Smoke/Unit/Compute.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @copyright Copyright 2012-2013 Rackspace US, Inc. 
+  See COPYING for licensing information.
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache 2.0
+ * @version   1.5.9
+ * @author    Glen Campbell <glen.campbell@rackspace.com>
+ * @author    Jamie Hannaford <jamie.hannaford@rackspace.com>
+ */
+
+/**
+ * Description of Compute
+ * 
+ * @link 
+ */
+class Compute
+{
+    
+}
