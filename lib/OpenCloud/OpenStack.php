25e299045b1bef2161af3a956a74e58a8713b816
Justin Phillips <jjustinphillipss@gmail.com>
2013-09-18 19:12:51 -0700

    see json too

--- lib/OpenCloud/OpenStack.php
+++ lib/OpenCloud/OpenStack.php
@@ -741,6 +741,7 @@ class OpenStack extends Base
         $object = json_decode($json);
         $this->checkJsonError();
         
+        printf($json . " the json \n");
         printf($object . " hope this works\n");
         // Save the token information as well as the ServiceCatalog
         $this->setToken($object->access->token->id);
