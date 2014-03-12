06d9ea000c0368402039063e28e438ee40a53ee6
Shaunak Kashyap <ycombinator@gmail.com>
2014-03-11 17:25:21 -0700

    Adding description for Stack resource class.

--- lib/OpenCloud/Orchestration/Resource/Stack.php
+++ lib/OpenCloud/Orchestration/Resource/Stack.php
@@ -22,13 +22,13 @@ use OpenCloud\Common\Lang;
 use OpenCloud\Common\Resource\PersistentResource;
 
 /**
- * A stack is a group of resources (servers, load balancers, databases, and so 
- * forth) combined to fulfill a useful purpose. Based on a template, Heat 
- * orchestration engine creates an instantiated set of resources (a stack) to 
- * run the application framework or component specified (in the template). A 
+ * A stack is a group of resources (servers, load balancers, databases, and so
+ * forth) combined to fulfill a useful purpose. Based on a template, Heat
+ * orchestration engine creates an instantiated set of resources (a stack) to
+ * run the application framework or component specified (in the template). A
  * stack is a running instance of a template. The result of creating a stack is
  * a deployment of the application framework or component.
- * 
+ *
  */
 class Stack extends PersistentResource
 {
