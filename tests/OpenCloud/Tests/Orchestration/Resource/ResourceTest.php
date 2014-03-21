b24cf1e94f05741192dc21d909ad0bb9db7eece8
Shaunak Kashyap <ycombinator@gmail.com>
2014-03-20 22:27:44 -0700

    Adding more unit tests for Resource resource class.

--- tests/OpenCloud/Tests/Orchestration/Resource/ResourceTest.php
+++ tests/OpenCloud/Tests/Orchestration/Resource/ResourceTest.php
@@ -27,4 +27,53 @@ class ResourceTest extends OrchestrationTestCase
         $this->assertInstanceOf('OpenCloud\Orchestration\Resource\Resource', $this->resource);
     }
 
+    public function testGetResourceName()
+    {
+        $this->assertEquals('my_instance', $this->resource->getResourceName());
+    }
+
+    public function testGetLogicalResourceId()
+    {
+        $this->assertEquals('my_instance', $this->resource->getLogicalResourceId());
+    }
+
+    public function testGetPhysicalResourceId()
+    {
+        $this->assertEquals('7fdb0a8a-6514-455c-b5cf-96a131b71352', $this->resource->getPhysicalResourceId());
+    }
+
+    public function testGetResourceStatus()
+    {
+        $this->assertEquals('CREATE_COMPLETE', $this->resource->getResourceStatus());
+    }
+
+    public function testGetResourceStatusReason()
+    {
+        $this->assertEquals('state changed', $this->resource->getResourceStatusReason());
+    }
+
+    public function testGetResourceType()
+    {
+        $this->assertEquals('OS::Nova::Server', $this->resource->getResourceType());
+    }
+
+    public function testGetUpdatedTime()
+    {
+        $this->assertEquals('2014-03-20T14:27:15Z', $this->resource->getUpdatedTime());
+    }
+
+    public function testGetRequiredBy()
+    {
+        $requiredBy = $this->resource->getRequiredBy();
+        $this->assertTrue(is_array($requiredBy));
+        $this->assertEquals(0, count($requiredBy));
+    }
+
+    public function testGetLinks()
+    {
+        $links = $this->resource->getLinks();
+        $this->assertTrue(is_array($links));
+        $this->assertEquals(2, count($links));
+    }
+
 }
