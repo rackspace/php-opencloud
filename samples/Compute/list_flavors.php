a81ca887e65301d437ccf942e56e840948276fd6
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-11-19 11:16:30 +0100

    Add list scripts

---
+++ samples/Compute/list_flavors.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright 2012-2014 Rackspace US, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+require dirname(__DIR__) . '/../vendor/autoload.php';
+
+use OpenCloud\Rackspace;
+
+// 1. Instantiate a Rackspace client. You can replace {authUrl} with
+// Rackspace::US_IDENTITY_ENDPOINT or similar
+$client = new Rackspace('{authUrl}', array(
+    'username' => '{username}',
+    'apiKey'   => '{apiKey}',
+));
+
+// 2. Obtain a Compute service object from the client.
+$computeService = $client->computeService(null, '{region}');
+
+// 3. Get the list of flavors.
+$images = $service->flavorList();
+
+// 4. Traverse
+foreach ($flavors as $flavor) {
+    printf("ID: %s, Name: %s\n", $flavor->id, $flavor->name);
+}
