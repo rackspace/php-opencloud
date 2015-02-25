087e774b639d257c5c2d28ffb3a085f058f58ae2
Shaunak Kashyap <ycombinator@gmail.com>
2015-02-24 22:22:30 -0800

    Provide method to prefix user agent.

---
+++ tests/OpenCloud/Tests/Common/Http/ClientTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright 2012-2014 Rackspace US, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OpenCloud\Tests\Common\Http;
+
+use OpenCloud\Tests\OpenCloudTestCase;
+use OpenCloud\Common\Http\Client;
+
+class ClientTest extends OpenCloudTestCase
+{
+    public function testPrependUserAgent()
+    {
+        $client = new Client();
+
+        $originalUserAgent = $client->getUserAgent();
+        $prefix = "users-app/0.1.2";
+        $newUserAgent = $prefix . " " . $originalUserAgent;
+
+        $client->prefixUserAgent($prefix);
+        $this->assertEquals($newUserAgent, $client->getUserAgent());
+    }
+}
