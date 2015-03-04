1647471543be0aff7a202c3e5a21e856a9c2a9ad
Jamie Hannaford <jamie.hannaford@rackspace.com>
2015-03-04 13:50:03 +0100

    Turn on extension

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ lexers['php'] = PhpLexer(startinline=True, linenos=1)
 lexers['php-annotations'] = PhpLexer(startinline=True, linenos=1)
 primary_domain = 'php'
 
-extensions = []
+extensions = ['sphinxcontrib.phpdomain']
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
