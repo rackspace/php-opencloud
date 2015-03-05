4bfdc0185d974b651b82760bc3b77272bfc21a61
Shaunak Kashyap <ycombinator@gmail.com>
2015-03-04 23:24:17 -0800

    Adding downloadable code samples for Security Groups and Security Group Rules.

---
+++ samples/Networking/list-security-group-rules.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright 2012-2014 Rackspace US, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+require dirname(__DIR__) . '/../vendor/autoload.php';
+
+use OpenCloud\Rackspace;
+
+// 1. Instantiate a Rackspace client. You can replace {authUrl} with
+// Rackspace::US_IDENTITY_ENDPOINT or similar
+$client = new Rackspace('{authUrl}', array(
+    'username' => '{username}',
+    'apiKey'   => '{apiKey}',
+));
+
+// 2. Obtain a Networking service object from the client.
+$networkingService = $client->networkingService(null, '{region}');
+
+// 3. List security group rules
+$securityGroupRules = $networkingService->listSecurityGroupRules();
+foreach ($securityGroupRules as $securityGroupRule) {
+    /** @var $securityGroupRule OpenCloud\Networking\Resource\SecurityGroupRule **/
+}
