975866eec02c86573c03a8d0616a5ed15dcb26af
Shaunak Kashyap <ycombinator@gmail.com>
2015-03-13 11:07:32 -0700

    Bumping up patch version for release.

--- lib/OpenCloud/Version.php
+++ lib/OpenCloud/Version.php
@@ -27,7 +27,7 @@ use Guzzle\Http\Curl\CurlVersion;
  */
 class Version
 {
-    const VERSION = '1.12.1';
+    const VERSION = '1.12.2';
 
     /**
      * @return string Indicate current SDK version.
