736fd50977b5644a1008abac6d07037d34ed666e
Jamie Hannaford <jamie.hannaford@rackspace.com>
2016-02-12 14:27:27 +0100

    add service files for RackConnect

---
+++ src/RackConnect/v3/Api.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace Rackspace\RackConnect\v3;
+
+use OpenStack\Common\Api\AbstractApi;
+
+class Api extends AbstractApi
+{
+    private $params;
+
+    public function __construct()
+    {
+        $this->params = new Params;
+    }
+
+    /**
+     * Returns information about GET /v3/public_ips HTTP operation
+     *
+     * @return array
+     */
+    public function getPublicIps()
+    {
+        return [
+            'method' => 'GET',
+            'path'   => 'public_ips',
+            'params' => [],
+        ];
+    }
+
+    /**
+     * Returns information about POST /v3/public_ips HTTP operation
+     *
+     * @return array
+     */
+    public function postPublicIps()
+    {
+        return [
+            'method'  => 'POST',
+            'path'    => '/v3/public_ips',
+            'jsonKey' => 'cloud_server',
+            'params'  => [
+                'id' => $this->params->idJson(),
+            ],
+        ];
+    }
+
+    /**
+     * Returns information about GET /v3/public_ips/{public_IP_id} HTTP operation
+     *
+     * @return array
+     */
+    public function getPublicIPId()
+    {
+        return [
+            'method' => 'GET',
+            'path'   => '/v3/public_ips/{public_IP_id}',
+            'params' => [
+                'publicIPId' => $this->params->publicIPIdUrl(),
+            ],
+        ];
+    }
+
+    /**
+     * Returns information about DELETE /v3/public_ips/{public_IP_id} HTTP operation
+     *
+     * @return array
+     */
+    public function deletePublicIPId()
+    {
+        return [
+            'method' => 'DELETE',
+            'path'   => '/v3/public_ips/{public_IP_id}',
+            'params' => [
+                'publicIPId' => $this->params->publicIPIdUrl(),
+            ],
+        ];
+    }
+
+    /**
+     * Returns information about GET cloud_networks HTTP operation
+     *
+     * @return array
+     */
+    public function getCloudNetworks()
+    {
+        return [
+            'method' => 'GET',
+            'path'   => 'cloud_networks',
+            'params' => [],
+        ];
+    }
+
+    /**
+     * Returns information about GET /v3/public_ips/cloud_server_id HTTP operation
+     *
+     * @return array
+     */
+    public function getCloudServerId()
+    {
+        return [
+            'method' => 'GET',
+            'path'   => 'public_ips/cloud_server_id',
+            'params' => [],
+        ];
+    }
+
+    /**
+     * Returns information about GET load_balancer_pools HTTP operation
+     *
+     * @return array
+     */
+    public function getLoadBalancerPools()
+    {
+        return [
+            'method' => 'GET',
+            'path'   => 'load_balancer_pools',
+            'params' => [],
+        ];
+    }
+
+    /**
+     * Returns information about POST load_balancer_pools/nodes HTTP
+     * operation
+     *
+     * @return array
+     */
+    public function postNodes()
+    {
+        return [
+            'method'  => 'POST',
+            'path'    => 'load_balancer_pools/nodes',
+            'jsonKey' => 'cloud_server',
+            'params'  => [
+                'id' => $this->params->idJson(),
+            ],
+        ];
+    }
+
+    /**
+     * Returns information about GET load_balancer_pools/nodes HTTP
+     * operation
+     *
+     * @return array
+     */
+    public function getNodes()
+    {
+        return [
+            'method' => 'GET',
+            'path'   => 'load_balancer_pools/nodes',
+            'params' => [],
+        ];
+    }
+
+    /**
+     * Returns information about GET cloud_networks/{network_id} HTTP
+     * operation
+     *
+     * @return array
+     */
+    public function getNetworkId()
+    {
+        return [
+            'method' => 'GET',
+            'path'   => 'cloud_networks/{network_id}',
+            'params' => [
+                'networkId' => $this->params->networkIdUrl(),
+            ],
+        ];
+    }
+
+    /**
+     * Returns information about GET load_balancer_pools/details HTTP
+     * operation
+     *
+     * @return array
+     */
+    public function getDetails()
+    {
+        return [
+            'method' => 'GET',
+            'path'   => 'load_balancer_pools/details',
+            'params' => [],
+        ];
+    }
+
+    /**
+     * Returns information about GET
+     * load_balancer_pools/{load_balancer_pool_id} HTTP operation
+     *
+     * @return array
+     */
+    public function getLoadBalancerPoolId()
+    {
+        return [
+            'method' => 'GET',
+            'path'   => 'load_balancer_pools/{load_balancer_pool_id}',
+            'params' => [
+                'loadBalancerPoolId' => $this->params->loadBalancerPoolIdUrl(),
+            ],
+        ];
+    }
+}
---
+++ src/RackConnect/v3/Params.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Rackspace\RackConnect\v3;
+
+use OpenStack\Common\Api\AbstractParams;
+
+class Params extends AbstractParams
+{
+    /**
+     * Returns information about id parameter
+     *
+     * @return array
+     */
+    public function idJson()
+    {
+        return [
+            'type'     => self::STRING_TYPE,
+            'location' => self::JSON,
+        ];
+    }
+
+    /**
+     * Returns information about publicIPId parameter
+     *
+     * @return array
+     */
+    public function publicIPIdUrl()
+    {
+        return [
+            'type'     => self::STRING_TYPE,
+            'location' => self::URL,
+            'sentAs'   => 'public_IP_id',
+        ];
+    }
+
+    /**
+     * Returns information about tenantId parameter
+     *
+     * @return array
+     */
+    public function tenantIdUrl()
+    {
+        return [
+            'type'     => self::STRING_TYPE,
+            'location' => self::URL,
+            'sentAs'   => 'tenant_id',
+        ];
+    }
+
+    /**
+     * Returns information about networkId parameter
+     *
+     * @return array
+     */
+    public function networkIdUrl()
+    {
+        return [
+            'type'     => self::STRING_TYPE,
+            'location' => self::URL,
+            'sentAs'   => 'network_id',
+        ];
+    }
+
+    /**
+     * Returns information about loadBalancerPoolId parameter
+     *
+     * @return array
+     */
+    public function loadBalancerPoolIdUrl()
+    {
+        return [
+            'type'     => self::STRING_TYPE,
+            'location' => self::URL,
+            'sentAs'   => 'load_balancer_pool_id',
+        ];
+    }
+
+
+}
